a6ff2786169dd43ef95282d0b8fcec99ffa4bb33
alexismoreaulloberauser.email <alexis.moreau.llobera@gmail.com>
2022-06-07 13:27:30 +0200

    Création des vue détaillées et listée avec lien

--- hackathon/application/views.py
+++ hackathon/application/views.py
@@ -7,9 +7,17 @@ def object_list (request) :
     objects = Object.objects.all()
     return render (request, 'application/object_list.html', {'objects': objects})
 
+def object_details(request, id):
+    object = Object.objects.get(id=id)
+    return render (request, 'application/object_details.html', {'object' : object})
+
+
 
 def object2_list (request) :
     objects = Object2.objects.all()
     return render (request, 'application/object2_list.html', {'objects': objects})
 
+def object2_details(request, id):
+    object = Object2.objects.get(id=id)
+    return render (request, 'application/object2_details.html', {'object' : object})
 
