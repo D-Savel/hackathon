46b9a8df37571411b3edf49af26fc5e09c6cda56
D-Savel <dad.savel@gmail.com>
2022-06-08 20:36:36 +0200

    add fetch api food

--- hackathon/application/views.py
+++ hackathon/application/views.py
@@ -18,29 +18,21 @@ environ.Env.read_env()
 # Create your views here.
 
 
-def geoapi(request):
+def fetchApi(request):
 
-    ip_address = request.META.get('HTTP_X_FORWARDED_FOR', '')
-    # use api with your ip Address for retrieve geodata
-    response = requests.get('http://ip-api.com/json/%s' % ip_address)
-    geodata = response.json()
-    lat = geodata['lat']
-    lon = geodata['lon']
-    API_KEY = env("API_KEY")
-    # prepare url for display a map with api
-    # API use api-key given by locationiq
-    # API_KEY must be save in .env file at the root of the project (same as settings.py)
-    # API_KEY=<YOUR_API_KEY> (without '')
-    url = f"""https://maps.locationiq.com/v3/staticmap?key={API_KEY}&center={lat},{lon}&size=800x800&zoom=13&markers=size:small|color:red|{lat},{lon}"""
-
-    return render(request, 'application/geoapi.html', {
-        'ip': geodata['query'],
-        'country': geodata['country'],
-        'latitude': geodata['lat'],
-        'longitude': geodata['lon'],
-        'city': geodata['city'],
-        'url': url
-    })
+    url = f"""https://fr.openfoodfacts.org/cgi/search.pl?search_terms=pate+a+tartiner&search_simple=1&action=process&json=1"""
+    response = requests.get(url)
+    response= response.json()
+    productList = response['products']
+    productKeywords = []
+    productKeywordsList = []
+    for product in productList :
+        productKeywords = product['product_name_fr'].split(" ")
+        productKeywordsList.append(productKeywords[0])
+   
+    return render(request, 'application/fetchApi.html', {
+        'productKeywordsList': productKeywordsList,
+            })
     
 def requestdb (request):
     conn = mysql.connector.connect(host="51.158.97.130",
